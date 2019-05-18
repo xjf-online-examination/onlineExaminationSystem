443112c89dc54dad2d4d1e43d2935f6179d39488
wangxiaojun <193014656@qq.com>
2019-05-18 13:55:59 +0800

    no message

--- JAVA/onlineExamination/src/main/java/com/wxj/model/Bean/RequestBean.java
+++ JAVA/onlineExamination/src/main/java/com/wxj/model/Bean/RequestBean.java
@@ -15,15 +15,12 @@ public class RequestBean <T> implements Serializable{
 	private static final long serialVersionUID = 484604014474085210L;
 
 	@JSONField(ordinal = 1)
-	private String username;
-
-	@JSONField(ordinal = 2)
 	private String securityKey;
 	
-	@JSONField(ordinal = 3)
-	private String timestamp;
+	@JSONField(ordinal = 2)
+	private String userCode;
 
-	@JSONField(ordinal = 4)
+	@JSONField(ordinal = 3)
 	private T data;
 
 }
